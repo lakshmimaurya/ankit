7d619d2edebf9ae3c22da9816c5c7ec290888ea1
Lakshmi Maurya <55014498+lakshmimaurya@users.noreply.github.com>
2021-10-02 00:32:50 +0530

    Revert "Updated the project to latest structure"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,32 +8,36 @@ import { HomeComponent } from './pages/home/home.component';
 import { PublicationComponent } from './pages/publication/publication.component';
 import { StudentComponent } from './pages/student/student.component';
 import { TeamComponent } from './pages/team/team.component';
-import { combineLatest } from 'rxjs';
-import { AdminComponent } from './admin/admin.component';
-import { AdminloginComponent } from './auth/adminlogin/adminlogin.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { AuthGuard } from './auth.guard';
-
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: "home", component: HomeComponent },
-  { path: "about", component: AboutComponent },
-  { path: "events", component: EventsComponent },
-  { path: "publish", component: PublicationComponent },
-  { path: "team", component: TeamComponent },
-  { path: "student", component: StudentComponent },
-  { path: "contact", component: ContactComponent },
-  { path: "convention", component: ConventionComponent },
-  { path: "admin", component: AdminloginComponent },
-  { path: "create", component: SignupComponent },
-  { path: "dashboard", component: AdminComponent, canActivate: [AuthGuard] },
-  { path: "**", redirectTo: 'home' }
+  {
+    path: "", component: HomeComponent
+  },
+  {
+    path: "about", component: AboutComponent
+  },
+  {
+    path: "events", component: EventsComponent
+  },
+  {
+    path: "publish", component: PublicationComponent
+  },
+  {
+    path: "team", component: TeamComponent
+  },
+  {
+    path: "student", component: StudentComponent
+  },
+  {
+    path: "contact", component: ContactComponent
+  },
+  {
+    path: "convention", component: ConventionComponent
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy', scrollPositionRestoration: 'top' })],
-  exports: [RouterModule],
-  providers: [AuthGuard]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
